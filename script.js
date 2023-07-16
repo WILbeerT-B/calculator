6d62d4364bed25bc0e3205508195dc426365fa4b
WILbeerT-B <wilbertbundalian@gmail.com>
2023-07-15 19:17:41 -0700

    Modify all files
    HTML - change display-containers attribute from CLASS to ID.
    CSS - change BG color and font color.
    JS - add another display for the solution.

--- script.js
+++ script.js
@@ -1,41 +1,39 @@
 let displayValue = '0',
+    displayValue1 = '',
     firstOperand = null,
     secondOperand = null,
     operator = null,
-    result = null;
+    result = null,
+    roundResult = null;
 const buttons = document.querySelectorAll('button');
 
-function add(a, b) {
-    return a + b;
-}
-
-function subtract(a, b) {
-    return a - b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
 function operate(op, num1, num2) {
-    if (op === "+") {
-        return add(Number(num1), Number(num2));
-    } else if (op === "-") {
-        return subtract(num1, num2);
-    } else if (op === "/") {
-        return divide(num1, num2);
-    } else if (op === "*") {
-        return multiply(num1, num2);
+    if(op === "+") {
+        return num1 + num2;
+    } else if(op === "-") {
+        return num1 - num2
+    } else if(op === "/") {
+        if(num2 === 0) {
+            return 'ERROR';
+        } else {
+            return num1 / num2;
+        }
+    } else if(op === "*") {
+        return num1 * num2;
     }
 }
 
 function updateDisplay() {
-    const displayContainer2 = document.querySelector('.display-container2');
-    displayContainer2.textContent = displayValue;
+    const displayContainer = document.getElementById('display-container');
+    displayContainer.innerText = displayValue;
+    const displayContainer1 = document.getElementById('display-container1');
+    displayContainer1.innerText = displayValue1;
+    if(displayValue.length > 9) {
+        displayContainer.textContent = displayValue.substring(0, 9);
+    }
+    if(displayValue1.length > 10) {
+        displayContainer1.textContent = displayValue1.substring(0, 10);
+    }
 }
 updateDisplay();
 
@@ -54,6 +52,9 @@ function clickButton() {
             } else if(buttons[i].classList.contains('clear')) {
                 clearDisplay();
                 updateDisplay();
+            } else if(buttons[i].classList.contains('decimal')) {
+                inputDecimal(buttons[i].value);
+                updateDisplay();
             }
         })
     }
@@ -64,22 +65,29 @@ function inputOperand(operand) {
     if(firstOperand === null) {
         if(displayValue === '0' || displayValue === 0) {
             displayValue = operand;
+            displayValue1 = operand;
         } else if(displayValue === firstOperand) {
             displayValue = operand;
+            displayValue1 = operand;
         } else if(displayValue === result) {
-            displayValue = operand; 
+            displayValue = operand;
         } else {
             displayValue += operand;
+            displayValue1 += operand;
         }
     } else {
         if(displayValue === '0' || displayValue === 0) {
             displayValue = operand;
+            displayValue1 += operand;
         } else if(displayValue === firstOperand) {
             displayValue = operand;
-        } else if(displayValue === result) {
+            displayValue1 += operand;
+        } else if(displayValue === roundResult) {
             displayValue = operand;
+            displayValue1 += operand;
         } else {
             displayValue += operand;
+            displayValue1 += operand;
         }
     }
 }
@@ -88,11 +96,15 @@ function inputOperator(op) {
     if(firstOperand === null) {
         firstOperand = displayValue;
         operator = op;
+        displayValue1 = displayValue + operator;
     } else {
         secondOperand = displayValue;
-        result = operate(operator, firstOperand, secondOperand);
-        displayValue = result;
+        displayValue1 = displayValue + operator;
+        result = operate(operator, Number(firstOperand), Number(secondOperand));
         operator = op;
+        roundResult = roundNumber(result, 15).toString();
+        displayValue = roundResult;
+        displayValue1 = roundNumber(result, 15).toString() + operator;
         firstOperand = result;
     }
 }
@@ -100,18 +112,42 @@ function inputOperator(op) {
 function inputEquals() {
     if(operator !== null) {
         secondOperand = displayValue;
-        result = operate(operator, firstOperand, secondOperand);
-        displayValue = result;
-        firstOperand = null;
+        result = operate(operator, Number(firstOperand), Number(secondOperand));
+        if(result === 'ERROR') {
+            displayValue = 'ERROR';
+            displayValue1 = '';
+        } else {
+            roundResult = roundNumber(result, 15).toString();
+            displayValue = roundResult;
+            firstOperand = null;
+        }
     } else {
-        displayValue = '0';
+        displayValue = displayValue;
+        displayValue1 = displayValue1;
     }
 }
 
 function clearDisplay() {
     displayValue = '0';
+    displayValue1 = '';
     firstOperand = null;
     secondOperand = null;
     operator = null;
 }
 clearDisplay();
+
+function inputDecimal(dot) {
+    if(displayValue === firstOperand || displayValue === secondOperand) {
+        displayValue = '0';
+        displayValue1 = '0';
+        displayValue += dot;
+        displayValue1 += dot;
+    } else if(!displayValue.includes(dot)) {
+        displayValue += dot;
+        displayValue1 += dot;
+    }
+}
+
+function roundNumber(num, places) {
+    return parseFloat(Math.round(num + 'e' + places) + 'e-' + places);
+}
